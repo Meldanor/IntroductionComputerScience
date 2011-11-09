833407ec07f3e8b0f7f7a8864031f6422445833d
Meldanor <Bhaals@gmx.de>
2011-11-09 10:32:19 +0100

    Add constructor...like a boss
    Signed-off-by: Meldanor <Bhaals@gmx.de>

--- src/training4/Rectangle.java
+++ src/training4/Rectangle.java
@@ -1,6 +1,10 @@
 package training4;
 
 public class Rectangle extends java.awt.Rectangle {
+    
+    public Rectangle(int x, int y, int width, int height) {
+        super(x, y, width, height);
+    }
 
     public boolean intersect(Rectangle r) {
 
